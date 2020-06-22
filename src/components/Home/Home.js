fcc17226c49aac647f036dd9023fca94003207a5
Caroline Ninganga <carolninganga@gmail.com>
2020-06-22 16:38:45 -0400

    added image to landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 
 function Home() {
     return (
         <div className="container">
+            <div id="HeroWrapper">
+            <div id="heroImg" className="row" style={{background: 'url(https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2Fvr-headset-removebg.png?v=1592856275884) center / cover'}}></div>
+            </div>
             <h4 className="center" id="heading">
                 Katabi
             </h4>
