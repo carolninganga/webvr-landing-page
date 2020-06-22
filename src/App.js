aeae48b2d739ce976e08b3ac9808449209e86d88
Caroline Ninganga <carolninganga@gmail.com>
2020-06-22 11:12:29 -0400

    added routes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
+
+    </BrowserRouter>
+  )
+    
+}
+
+export default App;
