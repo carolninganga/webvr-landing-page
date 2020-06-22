e021bb3aa51bbdf93e32e38194113e77136bd629
Caroline Ninganga <carolninganga@gmail.com>
2020-06-22 14:44:53 -0400

    changed styling on landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './style.css'
 
 function Home() {
     return (
         <div className="container">
-            <h4 className="center">
+            <h4 className="center" id="heading">
                 Katabi
             </h4>
-            <p>Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="row">
+                <div className="input-field col s12">
+                <input type="text" className="eventInfo" />
+                <label id="inputLabel" for="text">Enter event code</label>
+            </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './style.css';
 
 function Navbar() {
     return (
-        <nav className="nav-wrapper red darken-3">
+        <nav>
             <div className="container">
-                <a href="/" className="brand-logo">Katabi</a>
+                {/* <a href="/" className="brand-logo"></a> */}
                 <ul className="right">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
